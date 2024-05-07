b79da86d28b2ff190b1b6ed2ef389846f735e1c5
thecoolmime <seth.jordan68@hotmail.com>
2024-05-06 21:57:58 -0600

    ITS ALIVE!

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+if __name__ == '__main__':
+    main()
