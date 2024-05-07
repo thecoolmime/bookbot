9bb6a665e26f8d1c55a3aaa648dfba677de15e6e
thecoolmime <seth.jordan68@hotmail.com>
2024-05-06 23:21:57 -0600

    Sloppy but at least it outputs the word count

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+    words = file_contents.split()
+    word_count = len(words)
+    print(word_count)
+
+
 
 if __name__ == '__main__':
     main()
