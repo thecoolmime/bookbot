fe3bccb73cc3d71ddd8399eac752859d63a2fdc9
thecoolmime <seth.jordan68@hotmail.com>
2024-05-06 23:33:13 -0600

    Added documentation in notes above main. Refactored code to be more manageable for future commits

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+# Main information that all application is derived from
+#split python file into three functions 1. main function to establish the data path along with putting together returned values to create desired outcome 2. get_num_words function is to give word count 
+# 3. get_book_text to open data and return the words in general to be utilized in main
+# 4. Added f string with more text to look more professional in output
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-    words = file_contents.split()
-    word_count = len(words)
-    print(word_count)
+    book_path= "books/frankenstein.txt"
+    file_contents = get_book_text(book_path)
+    num_words = get_num_words(file_contents)
+    print(f"{num_words} words found in the document")
+    
 
+def get_num_words(file_contents):
+    words = file_contents.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 if __name__ == '__main__':
